849edc5fd830b593b0d5a1dfc33cc82a9c17804c
Epsilon Delta <ivangladkikh4@gmail.com>
2025-09-07 14:37:16 +0700

    Hardhat migration

---
+++ src/mock_ynethx_action.ts
@@ -0,0 +1,63 @@
+import {
+  ActionFn, Context,
+  Event, BlockEvent
+} from "@tenderly/actions";
+ 
+// importing ethers available in Tenderly Runtime
+import { ethers } from "ethers";
+
+export const actionFn: ActionFn = async (
+  context: Context, 
+  event: Event
+) => {
+  const abi = [
+    "function withdraw(uint256 assets, address receiver, address owner) external"
+  ];
+  const bufferAbi = [
+    "function maxWithdraw(address owner) external view returns (uint256)"
+  ];
+
+  const WITHDRAW_AMOUNT = ethers.utils.parseEther("0.0009");
+
+  const walletPk = await context.secrets.get('IVAN_PK');
+  
+  if (!walletPk) {
+    throw new Error("Secret CICADA_WALLET_PK is not set");
+  }
+
+  const gatewayURL = context.gateways.getGateway();
+  const provider = new ethers.providers.JsonRpcProvider(gatewayURL);
+
+  const bufferAddress = "0x45c3B59d53e2e148Aaa6a857521059676D5c0489";
+  const vaultAddress = "0x657d9ABA1DBb59e53f9F3eCAA878447dCfC96dCb";
+  const EulerBuffer = new ethers.Contract(bufferAddress, bufferAbi, provider);
+  const ynETHX = new ethers.Contract(vaultAddress, abi, provider);
+
+  // Read the buffer maxWithdraw balance
+  const maxWithdraw = await EulerBuffer.maxWithdraw(vaultAddress);
+  if (maxWithdraw < WITHDRAW_AMOUNT) {
+    throw new Error("Not enough funds in buffer to withdraw");
+  }
+
+  // Create a wallet instance
+  const wallet = new ethers.Wallet(walletPk ?? "", provider);
+  const wallet_address = await wallet.getAddress();
+  const ynETHXWithSigner = ynETHX.connect(wallet);
+
+  // Check that wallet has enough ynETHx balance
+  const balance = await ynETHX.balanceOf(wallet_address);
+  if (balance < WITHDRAW_AMOUNT) {
+    throw new Error("Not enough ynETHx balance to withdraw");
+  }
+
+  // Withdraw 20 ETH to the wallet address
+  const tx = await ynETHXWithSigner.withdraw(
+    WITHDRAW_AMOUNT,
+    wallet_address, // receiver
+    wallet_address  // owner
+  );
+
+  const receipt = await tx.wait();
+
+  console.log(receipt);
+}
