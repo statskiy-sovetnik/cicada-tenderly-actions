508fa6ebf6a77a44b4ab382645d75b3af2ffd03c
Epsilon Delta <ivangladkikh4@gmail.com>
2025-09-07 15:08:18 +0700

    test: Config + initial tests

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,40 +1,21 @@
 import type { HardhatUserConfig } from "hardhat/config";
 
 import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
-import { configVariable } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   plugins: [hardhatToolboxMochaEthersPlugin],
-  solidity: {
-    profiles: {
-      default: {
-        version: "0.8.28",
-      },
-      production: {
-        version: "0.8.28",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-    },
-  },
   networks: {
     hardhatMainnet: {
       type: "edr-simulated",
       chainType: "l1",
+      forking: {
+        url: "https://mainnet.infura.io/v3/4161a0b9d7f44716a5699a9e91be996a",
+        blockNumber: 23309842
+      },
     },
     hardhatOp: {
       type: "edr-simulated",
       chainType: "op",
-    },
-    sepolia: {
-      type: "http",
-      chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
     },
   },
 };
